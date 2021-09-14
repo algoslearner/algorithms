6ba9ed847eee10a6d22aa4851a49bfc75f440586
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2021-09-14 09:50:19 -0700

    Create 0016_reverse_string.py

---
+++ lc_study_plan/algorithms_1/0016_reverse_string.py
@@ -0,0 +1,42 @@
+'''
+344. Reverse String
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+ 
+
+Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+'''
+
+# TC : O(n)
+# SC : O(1)
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start = 0
+        end = len(s) - 1
+        while start <= end:
+            tmp = s[end]
+            s[end] = s[start]
+            s[start] = tmp
+            start += 1
+            end -= 1
