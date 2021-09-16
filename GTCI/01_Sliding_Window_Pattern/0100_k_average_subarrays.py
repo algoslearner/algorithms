36db6ab2d5cd99ee4dffdc2d34f829c58ad97bd1
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2021-09-16 11:20:38 -0700

    Create 0100_k_average_subarrays.py

---
+++ GTCI/01_Sliding_Window_Pattern/0100_k_average_subarrays.py
@@ -0,0 +1,49 @@
+'''
+Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
+
+Let’s understand this problem with a real input:
+
+Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
+Here, we are asked to find the average of all contiguous subarrays of size ‘5’ in the given array. Let’s solve this:
+
+For the first 5 numbers (subarray from index 0-4), the average is: (1+3+2+6-1)/5 => 2.2(1+3+2+6−1)/5=>2.2
+The average of next 5 numbers (subarray from index 1-5) is: (3+2+6-1+4)/5 => 2.8(3+2+6−1+4)/5=>2.8
+For the next 5 numbers (subarray from index 2-6), the average is: (2+6-1+4+1)/5 => 2.4(2+6−1+4+1)/5=>2.4
+…
+Here is the final output containing the averages of all contiguous subarrays of size 5:
+
+Output: [2.2, 2.8, 2.4, 3.6, 2.8]
+
+'''
+
+# brute force
+# TC : O(n*k)
+# SC : O(1)
+'''
+def find_averages_of_subarrays(K, arr):
+  result = []
+  for i in range(len(arr)-K+1):
+    # find sum of next 'K' elements
+    _sum = 0.0
+    for j in range(i, i+K):
+      _sum += arr[j]
+    result.append(_sum/K)  # calculate average
+
+  return result
+'''
+
+# sliding window
+# TC : O(n)
+# SC : O(1)
+def find_averages_of_subarrays(K, arr):
+  result = []
+  windowSum, windowStart = 0.0, 0
+  for windowEnd in range(len(arr)):
+    windowSum += arr[windowEnd]  # add the next element
+    # slide the window, we don't need to slide if we've not hit the required window size of 'k'
+    if windowEnd >= K - 1:
+      result.append(windowSum / K)  # calculate the average
+      windowSum -= arr[windowStart]  # subtract the element going out
+      windowStart += 1  # slide the window ahead
+
+  return result
