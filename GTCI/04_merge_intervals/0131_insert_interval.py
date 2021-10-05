dce1932c7f3f1da531b0318a72d9e4c84087e416
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2021-10-04 22:10:00 -0700

    Create 0131_insert_interval.py

---
+++ GTCI/04_merge_intervals/0131_insert_interval.py
@@ -0,0 +1,72 @@
+'''
+57. Insert Interval
+
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+Example 3:
+
+Input: intervals = [], newInterval = [5,7]
+Output: [[5,7]]
+Example 4:
+
+Input: intervals = [[1,5]], newInterval = [2,3]
+Output: [[1,5]]
+Example 5:
+
+Input: intervals = [[1,5]], newInterval = [2,7]
+Output: [[1,7]]
+ 
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 105
+intervals is sorted by starti in ascending order.
+newInterval.length == 2
+0 <= start <= end <= 105
+'''
+
+# TC : O(n)
+# SC : O(n)
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        i = 0
+        
+        # add to output all intervals that come before newInterval
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+        
+        # merge all intervals that overlap with newInterval
+        while i < len(intervals) and intervals[i][0] < newInterval[1]:
+            newInterval[0] = min(newInterval[0],intervals[i][0])
+            newInterval[1] = max(newInterval[1],intervals[i][1])
+            i += 1
+        
+        result.append(newInterval)
+        
+        # add all remaining intervals to output
+        while i < len(intervals):
+            result.append(intervals[i])
+            i += 1
+        
+        return result
+        
+        
