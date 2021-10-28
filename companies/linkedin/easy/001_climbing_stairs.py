68d152b1ffa14591a24574af3b37859538cb0de0
roshini-michael <88410184+roshini-michael@users.noreply.github.com>
2021-10-27 21:49:59 -0700

    Update 001_climbing_stairs.py

--- companies/linkedin/easy/001_climbing_stairs.py
+++ companies/linkedin/easy/001_climbing_stairs.py
@@ -36,10 +36,14 @@ Constraints:
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        dp = [0 for i in range(n+5)]   
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+        if n <= 2: return n
         
-        return dp[n]
+        current = 0
+        prev1 = 1
+        prev2 = 2
+        for i in range(3, n+1):
+            current = prev1 + prev2
+            prev1 = prev2
+            prev2 = current
+        return current
+        
